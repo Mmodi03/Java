c1508bf56fe8e724f7556eb0079f796d2f588a45
Mmodi03 <148218397+Mmodi03@users.noreply.github.com>
2024-06-13 12:26:03 +0530

    Add files via upload

---
+++ Interthreadcommunication/src/com/tnsif/InterThreadCommunication/Consumer.java
@@ -0,0 +1,26 @@
+package com.tnsif.InterThreadCommunication;
+
+public class Consumer extends Thread{
+	Q obj;
+
+	public Consumer(Q obj) {
+		this.obj = obj;
+		start();
+	}
+	
+	public void run()
+	{
+		while(true)
+		{
+			try
+			{
+				Thread.sleep(3000);
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
+			obj.get();
+		}
+	}
+}
